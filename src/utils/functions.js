1ed41c1b8612012d4e2ad6ef29354434d1be5a71
Atul Pant <atulpant@Atuls-MacBook-Air.local>
2024-06-15 16:54:21 +0530

    fix: random index generate function

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,9 +1,7 @@
 const { PROMPT_DUMMY_ANSWERS } = require("./constants");
 
 export const getRandomAnswer = () => {
-  const randomIndex = Math.floor(
-    Math.random() * PROMPT_DUMMY_ANSWERS.length + 1
-  );
+  const randomIndex = Math.floor(Math.random() * PROMPT_DUMMY_ANSWERS.length);
 
   return PROMPT_DUMMY_ANSWERS[randomIndex];
 };
